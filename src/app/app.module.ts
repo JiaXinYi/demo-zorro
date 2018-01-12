8b20dd70ede29959a5df2c9450cc1bdc5b8190d8
jiaxin <924463684@qq.com>
2018-01-12 18:50:31 +0800

    修改布局，表单，添加登录界面，实现页面route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { Routes, RouterModule } from '@angular/router';
+import { routes } from './routes/routes';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    LoginComponent
+    LoginComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
     FormsModule, ReactiveFormsModule,
-
+    RouterModule.forRoot(routes),
     NgZorroAntdModule.forRoot({ extraFontName: 'anticon', extraFontUrl: './assets/fonts/iconfont' })
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  validateForm: FormGroup;
 
-  ngOnInit() {
+  _submitForm() {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[ i ].markAsDirty();
+    }
+    if (this.validateForm.valid) {
+      console.log('Valid!');
+      console.log(this.validateForm.value);
+      this.router.navigate(['form']);
+    }
   }
 
+  constructor(private fb: FormBuilder, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      userName: [ null, [ Validators.required ] ],
+      password: [ null, [ Validators.required ] ],
+      remember: [ true ],
+    });
+  }
 }
